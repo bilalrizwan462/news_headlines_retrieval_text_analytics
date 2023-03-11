cc77a89efd0e5a5dddbb49e1030ae6a8e94e1bf1
bilalrizwan462 <bilal.rizwan2@gmail.com>
2023-03-11 19:36:58 +0500

    Update Headlines_topic_modeling_App.py

--- Headlines_topic_modeling_App.py
+++ Headlines_topic_modeling_App.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 
 import re, string
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
